8e887b27d68401e58ffe239fe96b430f5b3d9c17
evelyn <evelyn0401evelyn@gmail.com>
2024-09-19 10:11:49 +0800

    修改標題

--- WeddingPlanningReport/Models/Metadata/WeddingPlanMetadata.cs
+++ WeddingPlanningReport/Models/Metadata/WeddingPlanMetadata.cs
@@ -1,17 +1,27 @@
-﻿namespace WeddingPlanningReport.Models.Metadata
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WeddingPlanningReport.Models.Metadata
 {
     internal class WeddingPlanMetadata
     {
+        
         public int CaseId { get; set; }
 
+        [Display(Name = "會員ID")]
+        [Required(ErrorMessage = "請確實填寫會員ID")]
         public int MemberId { get; set; }
 
+        [Display(Name = "婚禮計劃書名稱")]
+        [Required(ErrorMessage = "婚禮計劃書名稱必填")]
         public string? WeddingName { get; set; }
 
+        [Display(Name = "婚禮雙方介紹")]
         public string? Introduction { get; set; }
 
+        [Display(Name = "婚禮時間")]
         public DateTime? WeddingTime { get; set; }
 
+        [Display(Name = "婚禮地點")]
         public string? WeddingLocation { get; set; }
     }
 }
