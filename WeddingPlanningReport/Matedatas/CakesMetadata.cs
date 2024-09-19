69ad30c303de17c011b0e568f670f0db0a350951
Kevin <kevinleehtt@hmail.com>
2024-09-19 12:04:33 +0800

    新增cake的頁面，並且增加CRUD

---
+++ WeddingPlanningReport/Matedatas/CakesMetadata.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WeddingPlanningReport.Matedatas
+{
+    internal class CakesMetadata
+    {
+        [Display(Name = "喜餅編號")]
+        public int CakeId { get; set; }
+        [Display(Name = "商店編號")]
+        public int ShopId { get; set; }
+        [Display(Name = "喜餅風格")]
+        public string? CakeStyles { get; set; }
+        [Display(Name = "喜餅名稱")]
+        public string? CakeName { get; set; }
+        [Display(Name = "喜餅圖片")]
+        public string? CakeImg { get; set; }
+        [Display(Name = "喜餅描述")]
+        public string? CakeDescription { get; set; }
+        [Display(Name = "喜餅價格")]
+        public int? CakePrice { get; set; }
+        [Display(Name = "上架狀態")]
+        public bool? CakeStatus { get; set; }
+        [Display(Name = "喜餅庫存")]
+        public int? CakeStock { get; set; }
+        [Display(Name = "喜餅備註")]
+        public string? CakeAnnotation { get; set; }
+        [Display(Name = "過敏原資訊")]
+        public string? AllergenInfo { get; set; }
+        [Display(Name = "喜餅內容")]
+        public string? CakeContent { get; set; }
+
+    }
+}
--- WeddingPlanningReport/Models/Cake.cs
+++ WeddingPlanningReport/Models/Cake.cs
@@ -1,31 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace WeddingPlanningReport.Models;
 
 public partial class Cake
 {
+   
     public int CakeId { get; set; }
-
     public int ShopId { get; set; }
-
     public string? CakeStyles { get; set; }
-
     public string? CakeName { get; set; }
-
     public string? CakeImg { get; set; }
-
     public string? CakeDescription { get; set; }
-
     public int? CakePrice { get; set; }
-
     public bool? CakeStatus { get; set; }
-
     public int? CakeStock { get; set; }
-
     public string? CakeAnnotation { get; set; }
-
     public string? AllergenInfo { get; set; }
-
     public string? CakeContent { get; set; }
 }
