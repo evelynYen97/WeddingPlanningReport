1f1ebfb22e4c06df83b507d1819e7ae5472fda72
evelyn <evelyn0401evelyn@gmail.com>
2024-09-19 21:52:28 +0800

    event metadata做好

---
+++ WeddingPlanningReport/Models/Metadata-閻/EventsMetadata.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WeddingPlanningReport.Models.Metadata
+{
+    internal class EventsMetadata
+    {
+        [Display(Name = "活動項目編號")]
+        public int EventId { get; set; }
+
+        [Display(Name = "婚禮計劃書編號")]
+        [Required(ErrorMessage = "請確實填寫婚禮計劃書編號")]
+        public int CaseId { get; set; }
+
+        [Display(Name = "活動項目名稱")]
+        [Required(ErrorMessage = "請確實填寫活動項目名稱")]
+        public string? EventName { get; set; }
+
+        [Display(Name = "活動開始時間")]
+        public DateTime? EventTime { get; set; }
+
+        [Display(Name = "活動地點")]
+        public string? EventLocation { get; set; }
+
+        [Display(Name = "活動備注")]
+        public string? EventNotes { get; set; }
+
+        [Display(Name = "活動地點圖片")]
+        public string? EventLocationImg { get; set; }
+
+        [Display(Name = "婚宴場地説明圖片")]
+        public string? EventVenueImg1 { get; set; }
+
+        [Display(Name = "婚宴場地説明圖片")]
+        public string? EventVenueImg2 { get; set; }
+    }
+}
---
+++ WeddingPlanningReport/Models/PartialClass-閻/Event.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WeddingPlanningReport.Models.Metadata;
+
+namespace WeddingPlanningReport.Models
+{
+    [ModelMetadataType(typeof(EventsMetadata))]
+    public partial class Event
+    {
+
+    }
+}
