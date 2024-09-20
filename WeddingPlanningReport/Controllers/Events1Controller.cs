8195f0c620929c2b8306a6edffcfaab9a4c778e8
evelyn <evelyn0401evelyn@gmail.com>
2024-09-20 23:07:23 +0800

    創建Event1 刪除Event  增加event1 edit的圖片上傳功能

--- WeddingPlanningReport/Controllers/Events1Controller.cs
+++ WeddingPlanningReport/Controllers/Events1Controller.cs
@@ -2,38 +2,33 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Humanizer.Localisation;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
-using Microsoft.CodeAnalysis.Operations;
 using Microsoft.EntityFrameworkCore;
 using WeddingPlanningReport.Models;
 
 namespace WeddingPlanningReport.Controllers
 {
-    public class EventsController : Controller
+    public class Events1Controller : Controller
     {
         private readonly WeddingPlanningContext _context;
+        private readonly IWebHostEnvironment _webHostEnvironment;
 
-        public EventsController(WeddingPlanningContext context)
+        public Events1Controller(WeddingPlanningContext context, IWebHostEnvironment webHostEnvironment)
         {
             _context = context;
+            _webHostEnvironment = webHostEnvironment;
         }
 
-        // GET: Events
-        public async Task<IActionResult> Index(int? id)
+        // GET: Events1
+        public async Task<IActionResult> Index()
         {
-            var eventItem =  _context.Events
-                                   .Where(e => e.CaseId == id);
-
-            if (eventItem == null)
-            {
-                return NotFound();
-            }
-
-            return View(eventItem );
+            return View(await _context.Events.ToListAsync());
         }
 
-        // GET: Events/Details/5
+        // GET: Events1/Details/5
         public async Task<IActionResult> Details(int? id)
         {
             if (id == null)
@@ -51,18 +46,18 @@ namespace WeddingPlanningReport.Controllers
             return View(@event);
         }
 
-        // GET: Events/Create
+        // GET: Events1/Create
         public IActionResult Create()
         {
             return View();
         }
 
-        // POST: Events/Create
+        // POST: Events1/Create
         // To protect from overposting attacks, enable the specific properties you want to bind to.
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("EventId,CaseId,EventName,EventTime,EventLocation,EventNotes,EventLocationImg,EventVenueImg1,EventVenueImg2")] Event @event)
+        public async Task<IActionResult> Create([Bind("EventId,CaseId,EventName,EventTime,EventLocation,EventNotes,EventLocationImg,EventVenueImg1,EventVenueImg2,IsDelete")] Event @event)
         {
             if (ModelState.IsValid)
             {
@@ -73,7 +68,7 @@ namespace WeddingPlanningReport.Controllers
             return View(@event);
         }
 
-        // GET: Events/Edit/5
+        // GET: Events1/Edit/5
         public async Task<IActionResult> Edit(int? id)
         {
             if (id == null)
@@ -89,12 +84,12 @@ namespace WeddingPlanningReport.Controllers
             return View(@event);
         }
 
-        // POST: Events/Edit/5
+        // POST: Events1/Edit/5
         // To protect from overposting attacks, enable the specific properties you want to bind to.
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("EventId,CaseId,EventName,EventTime,EventLocation,EventNotes,EventLocationImg,EventVenueImg1,EventVenueImg2")] Event @event)
+        public async Task<IActionResult> Edit(int id, [Bind("EventId,CaseId,EventName,EventTime,EventLocation,EventNotes,EventLocationImg,EventVenueImg1,EventVenueImg2,IsDelete")] Event @event, IFormFile? file)
         {
             if (id != @event.EventId)
             {
@@ -105,6 +100,30 @@ namespace WeddingPlanningReport.Controllers
             {
                 try
                 {
+                    string wwwRootPath = _webHostEnvironment.WebRootPath;
+                    if (file != null)
+                    {
+                        //string fileName = Guid.NewGuid().ToString() + Path.GetExtension(file.FileName);為圖片生成一個唯一的檔名 (Guid.NewGuid())
+                        string fileName = file.FileName;
+                        string productPath = Path.Combine(wwwRootPath, @"eventImg");
+
+                        // 防止檔名衝突，如果檔案已存在，可以加後綴或處理邏輯
+                        string filePath = Path.Combine(productPath, fileName);
+                        if (System.IO.File.Exists(filePath))
+                        {
+                            // 檔案已存在，這裡可以根據需求修改，例如在檔名後加上時間戳
+                            string fileExtension = Path.GetExtension(fileName);
+                            string fileNameWithoutExtension = Path.GetFileNameWithoutExtension(fileName);
+                            fileName = $"{fileNameWithoutExtension}{DateTime.Now:yyyyMMddHHmmss}{fileExtension}";
+                            filePath = Path.Combine(productPath, fileName);
+                        }
+                        // 儲存圖片到指定路徑
+                        using (var fileStream = new FileStream(filePath, FileMode.Create))
+                        {
+                            await file.CopyToAsync(fileStream);
+                        }
+                        @event.EventLocationImg = fileName;// 更新圖片屬性為上傳的檔案名
+                    }
                     _context.Update(@event);
                     await _context.SaveChangesAsync();
                 }
@@ -124,7 +143,7 @@ namespace WeddingPlanningReport.Controllers
             return View(@event);
         }
 
-        // GET: Events/Delete/5
+        // GET: Events1/Delete/5
         public async Task<IActionResult> Delete(int? id)
         {
             if (id == null)
@@ -142,7 +161,7 @@ namespace WeddingPlanningReport.Controllers
             return View(@event);
         }
 
-        // POST: Events/Delete/5
+        // POST: Events1/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> DeleteConfirmed(int id)
