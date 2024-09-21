939ba1ba5885b2bdf875db2d2a676fc1460e553b
evelyn <evelyn0401evelyn@gmail.com>
2024-09-21 12:40:46 +0800

    新增預算圖控制項，metadata做好， 活動edit的顯示圖片做到一半

---
+++ WeddingPlanningReport/Models/Metadata/BudgetChartMetadata.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WeddingPlanningReport.Models.Metadata
+{
+    internal class BudgetChartMetadata
+    {
+        public int BudgetChartId { get; set; }
+
+        [Display(Name = "會員")]
+        [Required(ErrorMessage = "請確實填寫會員編號")]
+        public int MemberId { get; set; }
+
+        [Display(Name = "圖表類型")]
+        public string? ChartType { get; set; }
+
+        [Display(Name = "圖表名稱")]
+        public string? ChartName { get; set; }
+
+        [Display(Name = "圖表單位")]
+        public string? ChartUnit { get; set; }
+    }
+}
