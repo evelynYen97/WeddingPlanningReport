21a45bee638587f409adc05ce04bc90ed06388f2
evelyn <evelyn0401evelyn@gmail.com>
2024-09-22 20:41:38 +0800

    網頁維護外觀調整完成

---
+++ WeddingPlanningReport/Controllers/WebsiteMaintain.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WeddingPlanningReport.Controllers
+{
+    public class WebsiteMaintain : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
