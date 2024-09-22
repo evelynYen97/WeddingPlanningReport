309646a18e99b3df3c731329194cb648c05f90dc
evelyn <evelyn0401evelyn@gmail.com>
2024-09-23 01:27:40 +0800

    首頁修到一半

--- WeddingPlanningReport/Controllers/HomeController.cs
+++ WeddingPlanningReport/Controllers/HomeController.cs
@@ -6,15 +6,22 @@ namespace WeddingPlanningReport.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly WeddingPlanningContext _context;
         private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, WeddingPlanningContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
         public IActionResult Index()
         {
+            ViewBag.CurrentYear = DateTime.Now.Year;
+            ViewBag.NumberV = _context.Venues.Count();
+            ViewBag.NumberCar=_context.Cars.Count();
+            ViewBag.NumberCake=_context.Cakes.Count();
+            ViewBag.NumberDishes= _context.Dishes.Count();
             return View();
         }
 
