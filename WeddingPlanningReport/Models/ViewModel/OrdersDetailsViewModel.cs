14ee4b19a93f46cd8846dd2ce37807fff265a6f7
evelyn <evelyn0401evelyn@gmail.com>
2024-09-28 15:35:54 +0800

    首頁注冊人數、活躍人數數據調用 圖表建立

--- WeddingPlanningReport/Models/ViewModel/OrdersDetailsViewModel.cs
+++ WeddingPlanningReport/Models/ViewModel/OrdersDetailsViewModel.cs
@@ -4,7 +4,10 @@
     {
         List<CakeOrder> CakeOrders { get; set; }
 
-
+        public int VenuesTotal { get; set; }
+        public int CakesTotal { get; set; }
+        public int DishesTotal { get; set; }
+        public int CarsTotal { get; set; }
         public int VenueOrdersTotal { get; set; }
         public int CakeOrdersTotal { get; set; }
         public int DishesOrdersTotal { get; set; }
@@ -12,5 +15,13 @@
         public int OtherOrdersTotal { get; set; }
         public int OrdersTotal { get; set; }
         public int MemberNumber { get; set; }
+
+        public int preferC { get; set; }
+        public int preferW { get; set; }
+
+        public int lastTwoMonthMember { get; set; }
+        public int lastMonthMember { get; set; }
+        public int thisMonthMember { get; set; }
+
     }
 }
